2d3155c23afe48de1a2fa82e008d1335f50d3a6b
Devil2706 <63907907+Devil2706@users.noreply.github.com>
2024-09-25 16:01:47 +0400

    c

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = 8080;
 
+app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
