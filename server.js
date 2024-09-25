dd82f8269442cfd4e0210eca5d29c4859b4d4b46
Devil2706 <63907907+Devil2706@users.noreply.github.com>
2024-09-25 16:06:12 +0400

    made changes to display index.html

--- server.js
+++ server.js
@@ -4,8 +4,17 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = 8080;
 
-app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
+client.connect()
+    .then(() => {
+        app.use(express.static(path.join(__dirname)));
+
+        app.get('/', (req, res) => {
+            res.sendFile(path.join(__direname, 'index.html'));
+        });
+
+        app.use(express.json());
+    })
 app.listen(port, () => console.log('server is on'));
